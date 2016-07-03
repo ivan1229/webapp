30ebbdc326870e2417a7ee63c71728c6c135e554
ivan1229 <ivan_yip@msn.com>
2016-07-04 01:02:27 +0800

    2nd commit

--- main.rb
+++ main.rb
@@ -4,18 +4,51 @@ require 'pry'
 
 set :sessions, true
 
-get "/" do
-  erb :set_name
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+  #correct for Aces
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+    
+    total
+  end
 end
 
-post '/set_name' do
+
+get "/" do
+  erb :new_player
+end
+
+post '/new_player' do
   session[:player_name] = params[:player_name]
   redirect '/game'
 end
 
 get '/game' do
-  session[:deck] = [['2', 'H'], ['3', 'D']]
+  # created a deck and put it in session
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  # deal cards
+  session[:dealer_cards] = []
   session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
   erb :game
